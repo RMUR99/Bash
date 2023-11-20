04a9ed337f35c33bf86783e7a78715cc77865a7d
Reema Almurayshid <85951306+RMUR99@users.noreply.github.com>
2023-11-21 01:17:50 +0300

    bash setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.3.5/tailwindcss-windows-x64.exe
+
+chmod +x tailwindcss-windows-x64.exe
+
+mv tailwindcss-windows-x64.exe tailwindcss
+
+
